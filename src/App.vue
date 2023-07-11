f7a5c6612f13d9a0284aeb3b3dfb6e37c5f8d97d
GLCVLL <g.ioele96@outlook.it>
2023-07-11 17:45:08 +0200

    made 2 components for AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script>
 import AppHeader from './components/Header/AppHeader.vue'
 import AppMain from './components/Main/AppMain.vue'
+import AppFooter from './components/Footer/AppFooter.vue'
 export default {
-  components: { AppHeader, AppMain },
+  components: { AppHeader, AppMain, AppFooter },
 }
 </script>
 
 <template>
   <AppHeader />
   <AppMain />
+  <AppFooter />
 </template>
 
 <style lang="scss">
---
+++ src/components/Footer/AppFooter.vue
@@ -0,0 +1,12 @@
+<script>
+import NewsletterSection from './NewsletterSection.vue';
+export default {
+    components: { NewsletterSection },
+}
+</script>
+
+<template>
+    <NewsletterSection />
+</template>
+
+<style scoped lang="scss"></style>
