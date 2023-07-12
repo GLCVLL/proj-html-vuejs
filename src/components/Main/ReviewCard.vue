51b5cd00404dfed48f9ff9e70204cb05f3495386
GLCVLL <g.ioele96@outlook.it>
2023-07-12 16:59:29 +0200

    fixed review section

---
+++ src/components/Main/ReviewCard.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+    props: {
+        stars: {
+            type: Number,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        altText: {
+            type: String,
+            required: true
+        },
+        subtitle: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="my-card mx-2">
+        <ul class="review-stars d-flex">
+            <li v-for="star in stars" :key="star">
+                <font-awesome-icon :icon="['fas', 'star']" />
+            </li>
+        </ul>
+        <h3>{{ title }}</h3>
+        <p>{{ content }}</p>
+        <div class="d-flex align-items-center">
+            <img :src="image" :alt="altText">
+            <div class="mx-2 text-f">
+                <h4>{{ subtitle }}</h4>
+                <p>{{ description }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/_vars.scss' as *;
+
+.my-card {
+    background-color: $bg-color;
+    padding-left: 20px;
+}
+
+.review-stars {
+    padding-left: 0;
+    color: $bg-secondary-color;
+}
+
+.text-f {
+
+    h4 {
+        font-size: 20px;
+    }
+
+    p {
+        font-size: 15px;
+    }
+}
+
+img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+}
+</style>
--- src/components/Main/ReviewSection.vue
+++ src/components/Main/ReviewSection.vue
@@ -1,12 +1,60 @@
 <script>
+import ReviewCard from './ReviewCard.vue'
 export default {
-
+    components: { ReviewCard },
+    data() {
+        return {
+            currentIndex: 0,
+            cards: [
+                // Array di oggetti cards
+                {
+                    id: 1,
+                    stars: 5,
+                    title: "Great Place",
+                    content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+                    image: "../../src/assets/img/t1.png",
+                    altText: "Image 1",
+                    subtitle: "Tobias May",
+                    description: "UI/UX Designer"
+                },
+                {
+                    id: 2,
+                    stars: 4,
+                    title: "Great Place",
+                    content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+                    image: "../../src/assets/img/t1.png",
+                    altText: "Image 1",
+                    subtitle: "Tobias May",
+                    description: "UI/UX Designer"
+                },
+            ]
+        };
+    },
+    computed: {
+        isLastCard() {
+            return this.currentIndex === this.cards.length - 1;
+        }
+    },
+    methods: {
+        setCurrentIndex(targetIndex) {
+            this.currentIndex = targetIndex;
+        },
+        goToNext() {
+            if (this.isLastCard) {
+                this.currentIndex = 0;
+            }
+            else this.currentIndex++;
+        },
+        goToPrev() {
+            this.currentIndex--;
+        },
+    },
 }
 </script>
 
 <template>
     <section>
-        <div class="container">
+        <div class="container slider">
             <div class="row d-flex">
                 <div class="col-md-4">
                     <div class="review">
@@ -29,7 +77,17 @@ export default {
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-8"></div>
+                <div class="col-md-8 d-flex px-1">
+                    <ReviewCard v-for="card in cards" :key="card.id" :stars="card.stars" :title="card.title"
+                        :content="card.content" :image="card.image" :altText="card.altText" :subtitle="card.subtitle"
+                        :description="card.description" />
+                </div>
+            </div>
+            <div id="prev" @click="goToPrev" v-show="currentIndex">
+                <img src="../../assets/img/left-arrow.png" />
+            </div>
+            <div id="next" @click="goToNext" v-show="!isLastCard">
+                <img src="../../assets/img/next.png" />
             </div>
         </div>
     </section>
@@ -40,6 +98,7 @@ export default {
 
 section {
     padding: 0 0 90px;
+    margin: 50px 0;
 }
 
 .review-text {
@@ -65,5 +124,34 @@ section {
 .review-stars {
     color: $bg-secondary-color;
     padding: 0 0 0;
+}
+
+.slider {
+    position: relative;
+}
+
+#prev,
+#next {
+    position: absolute;
+    z-index: 1;
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    right: 0;
+    bottom: -50px;
+
+    img {
+        width: 20px;
+        height: 20px;
+    }
+}
+
+#prev {
+    right: 50px;
 }
 </style>
